f4d97a834170a0a6173897a14e9fe90eeb83e68e
Dantul <danieltulus8@gmail.com>
2022-12-29 18:34:56 +0800

    add new code

--- routes.js
+++ routes.js
@@ -1,50 +1,11 @@
+const { addBookHandler } = require('./handler');
+
 const routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
-    },
-    {
-        method: 'GET',
-        path: '/books',
-        handler: (request, h) => {
-            return 'This is a book Page';
-            Response.h (OK)
-        },
-    },
-    {
-        method: 'POST',
-        path: '/books',
-        handler: (request, h) => {
-            return 'This is a POST Method for the book Page';
-        },
-    },
-    {
-        method: 'GET',
-        path: '/books/{bookId?}',
-        handler: (request, h) => {
-            const { name = "not found" } = request.params;
-            return `Book ID Number ${bookId}!`;
-        },
-    },
-    {
-        method: 'PUT',
-        path: '/books/{bookId?}',
-        handler: (request, h) => {
-            const { name = "not found" } = request.params;
-            return `Edit Book ID Number ${bookId}!`; 
-        },
-    },
-    {
-        method: 'DELETE',
-        path: '/books/{bookId?}',
-        handler: (request, h) => {
-            const { name = "not found" } = request.params;
-            return `Delete Book ID Number ${bookId}!`; 
-        },
-    },
-]
+	{
+		method: 'POST',
+		path: '/books',
+		handler: addBookHandler,
+	},
+];
 
 module.exports = routes;
